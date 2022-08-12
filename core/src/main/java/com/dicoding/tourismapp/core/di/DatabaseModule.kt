7d58665161a3bafcfd051ff05d9907d8a0774b03
dicky <dickyangga73@gmail.com>
2022-08-12 16:25:28 +0800

    ci

--- core/src/main/java/com/dicoding/tourismapp/core/di/DatabaseModule.kt
+++ core/src/main/java/com/dicoding/tourismapp/core/di/DatabaseModule.kt
@@ -14,6 +14,7 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 class DatabaseModule {
+    
     @Singleton
     @Provides
     fun provideDatabase(@ApplicationContext context: Context): TourismDatabase = Room.databaseBuilder(
